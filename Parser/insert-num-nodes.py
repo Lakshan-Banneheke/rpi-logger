a798cba11db4fba774f7150f1705fda18189d1b0
dhaura <dhaurapathirana@gmail.com>
2023-03-27 23:40:42 +0530

    Add insert-num-nodes.py

---
+++ Parser/insert-num-nodes.py
@@ -0,0 +1,10 @@
+import csv
+
+with open('../log-files/processed_data.csv', 'r') as processed_data_file, open('../log-files/complete_processed_data.csv', 'w', newline='') as complete_processed_data_file:
+    data_reader = csv.reader(processed_data_file)
+    complete_data_writer = csv.writer(complete_processed_data_file)
+
+    number_of_worker_nodes = 2
+    for data in data_reader:
+        data.insert(2, number_of_worker_nodes)
+        complete_data_writer.writerow(data)
